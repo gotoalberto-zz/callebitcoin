0d3513e19acbe5e6e400d01982d6e46d6ef49cd7
Alberto Gomez Toribio <gotoalberto@coinffeine.com>
2014-08-04 12:27:00 +0200

    Add random number to avoid cache.

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
     $lc = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 }
 if($lc == "es"){
-    header("location: spanish.html");
+    header("location: spanish.html?p=" . rand());
 } else {
-    header("location: english.html");
+    header("location: english.html?p=" . rand());
 }
 exit();
 ?>
